6535fe542564ede4cbde8d7fb94b65ca9e04efb7
Victor <pdopnex@gmail.com>
2019-01-27 13:53:00 +0300

    Изменил получение информации о подключении к базе данных

--- src/EducationSystem/EducationSystem.Database/Source/DatabaseRecorder.cs
+++ src/EducationSystem/EducationSystem.Database/Source/DatabaseRecorder.cs
@@ -9,16 +9,18 @@ namespace EducationSystem.Database.Source
     {
         public static void Register(IServiceCollection services, IConfiguration configuration)
         {
-            var database = configuration.GetSection("Database");
+            var database = configuration
+                .GetSection(nameof(Database))
+                .Get<Database>();
 
             var sb = new StringBuilder();
 
-            sb.Append($"database = {database.GetSection("Name").Value};");
-            sb.Append($"server = {database.GetSection("Host").Value};");
-            sb.Append($"port = {database.GetSection("Port").Value};");
+            sb.Append($"database = {database.Name};");
+            sb.Append($"server = {database.Host};");
+            sb.Append($"port = {database.Port};");
 
-            sb.Append($"user id = {database.GetSection("UserName").Value};");
-            sb.Append($"password = {database.GetSection("UserPassword").Value};");
+            sb.Append($"user id = {database.UserName};");
+            sb.Append($"password = {database.UserPassword};");
 
             services.AddDbContext<EducationSystemDatabaseContext>(x => x.UseMySQL(sb.ToString()));
         }
