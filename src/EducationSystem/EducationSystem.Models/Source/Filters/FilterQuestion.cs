52120291aae853667e94cc0439ba450b1ddd0534
Victor <pdopnex@gmail.com>
2019-03-29 22:58:49 +0300

    Добавил типы вопросов по умолчанию

--- src/EducationSystem/EducationSystem.Models/Source/Filters/FilterQuestion.cs
+++ src/EducationSystem/EducationSystem.Models/Source/Filters/FilterQuestion.cs
@@ -5,7 +5,12 @@ namespace EducationSystem.Models.Source.Filters
 {
     public class FilterQuestion : Filter
     {
-        public List<QuestionType> QuestionTypes { get; set; } = null;
+        public List<QuestionType> QuestionTypes { get; set; } = new List<QuestionType>
+        {
+            QuestionType.ClosedOneAnswer,
+            QuestionType.ClosedManyAnswers,
+            QuestionType.WithProgram
+        };
 
         public int Count { get; set; } = 10;
     }
