22905ed69285679eeff33c7be538b726749fde86
Victor <pdopnex@gmail.com>
2019-05-10 02:35:42 +0300

    Update QuestionValidatorClosedAnswer.cs

--- src/EducationSystem/EducationSystem.Implementations/Validators/Questions/QuestionValidatorClosedAnswer.cs
+++ src/EducationSystem/EducationSystem.Implementations/Validators/Questions/QuestionValidatorClosedAnswer.cs
@@ -67,10 +67,7 @@ namespace EducationSystem.Implementations.Validators.Questions
                     x.Status = AnswerStatus.Ignore;
             });
 
-            if (result.Answers.All(x => x.Status == null || x.Status == AnswerStatus.Right))
-                result.SetRight(true);
-
-            return result.SetRight(false);
+            return result.SetRight(result.Answers.All(x => x.Status == null || x.Status == AnswerStatus.Right));
         }
     }
 }
