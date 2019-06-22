d560e08724b8fbe4b5d43b883a9619df075e695a
Victor <pdopnex@gmail.com>
2019-06-22 21:58:46 +0300

    Добавил базовую реализацию взаимодействия с системой контроля знаний (тестирования) в виде SSO

---
+++ src/EducationSystem/EducationSystem.Models/LogInRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace EducationSystem.Models
+{
+    public sealed class LogInRequest
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Address { get; set; }
+
+        public static implicit operator TokenRequest(LogInRequest request)
+        {
+            return new TokenRequest(request.Email, request.Password);
+        }
+    }
+}
---
+++ src/EducationSystem/EducationSystem.WebApp/Source/Tamers/TamerInteraction.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using EducationSystem.Interfaces;
+using EducationSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace EducationSystem.WebApp.Source.Tamers
+{
+    [Route("interaction")]
+    public class TamerInteraction : Controller
+    {
+        private readonly ITokenGenerator _tokenGenerator;
+
+        private static string TestingSystemAddress = "http://www.web-test.ru";
+
+        private const string TokenCookieName = "Token";
+
+        public TamerInteraction(IConfiguration configuration, ITokenGenerator tokenGenerator)
+        {
+            _tokenGenerator = tokenGenerator;
+
+            TestingSystemAddress = configuration[nameof(TestingSystemAddress)];
+        }
+
+        [Route("login")]
+        public async Task<IActionResult> LogIn(LogInRequest request)
+        {
+            var response = await _tokenGenerator.GenerateTokenAsync(request);
+
+            Response.Cookies.Append(TokenCookieName, response.Token);
+
+            var url = string.IsNullOrWhiteSpace(request.Address)
+                ? TestingSystemAddress
+                : request.Address;
+
+            return Redirect(url);
+        }
+
+        [Route("logout")]
+        public IActionResult LogOut()
+        {
+            Response.Cookies.Delete(TokenCookieName);
+
+            return Redirect(TestingSystemAddress + "/logout");
+        }
+    }
+}
